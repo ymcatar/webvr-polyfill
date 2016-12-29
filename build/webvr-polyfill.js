2d601109370a100bccae740789d44d129e7a0419
Yiu Man Chi <ymcatar@gmail.com>
2016-12-29 23:16:05 +0800

    rebuilt

--- build/webvr-polyfill.js
+++ build/webvr-polyfill.js
@@ -2298,6 +2298,18 @@ var Viewers = {
     inverseCoefficients: [-0.33836704, -0.18162185, 0.862655, -1.2462051,
       1.0560602, -0.58208317, 0.21609078, -0.05444823, 0.009177956,
       -9.904169E-4, 6.183535E-5, -1.6981803E-6]
+  }),
+  CardboardKnockoff: new CardboardViewer({
+    id: 'Cardboard Knockoff',
+    label: 'Cardboard Knockoff',
+    fov: 50,
+    interLensDistance: 0.043,
+    baselineLensDistance: 0.035,
+    screenLensDistance: 0.038,
+    distortionCoefficients: [0.110, 0.070],
+    inverseCoefficients: [-0.33836704, -0.18162185, 0.862655, -1.2462051,
+      1.0560602, -0.58208317, 0.21609078, -0.05444823, 0.009177956,
+      -9.904169E-4, 6.183535E-5, -1.6981803E-6]
   })
 };
 
